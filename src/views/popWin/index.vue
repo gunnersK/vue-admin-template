ef9a68e3f821b0aebafdb8d7c37eb7e6446b2fa9
gunnersK <gunners_k@aliyun.com>
2020-11-02 13:18:55 +0800

    add: 弹窗新增框save

--- src/views/popWin/index.vue
+++ src/views/popWin/index.vue
@@ -127,72 +127,84 @@
 
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="fetchData" />
 
-    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible" center="true">
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible" width="650px">
       <div style="height: 500px; overflow: auto">
-      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="70px" style="width: 400px; margin-left:50px;">
-<!--        <el-form-item label="Type" prop="type">-->
-<!--          <el-select v-model="temp.type" class="filter-item" placeholder="Please select">-->
-<!--            <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />-->
-<!--          </el-select>-->
-<!--        </el-form-item>-->
-<!--        <el-form-item label="Date" prop="timestamp">-->
-<!--          <el-date-picker v-model="temp.timestamp" type="datetime" placeholder="Please pick a date" />-->
-<!--        </el-form-item>-->
-        <el-form-item label="弹窗名称" prop="name">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="弹窗类型" prop="winType">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="关联动态弹窗" prop="relatedId">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="用户类型" prop="userType">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="弹窗场景" prop="scene">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="弹窗次数" prop="time">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="频率（天）" prop="frequency">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="优先级" prop="priority">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="UI样式" prop="imgUrl">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="跳转地址" prop="desLink">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="总弹窗次数" prop="totalTime">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="前端数据" prop="data">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="最低可见版本号" prop="lowVersion">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="最高可见版本号" prop="highVersion">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-<!--        </el-form-item>-->
-<!--        <el-form-item label="Status">-->
-<!--          <el-select v-model="temp.status" class="filter-item" placeholder="Please select">-->
-<!--            <el-option v-for="item in statusOptions" :key="item" :label="item" :value="item" />-->
-<!--          </el-select>-->
-<!--        </el-form-item>-->
-<!--        <el-form-item label="Imp">-->
-<!--          <el-rate v-model="temp.importance" :colors="['#99A9BF', '#F7BA2A', '#FF9900']" :max="3" style="margin-top:8px;" />-->
-<!--        </el-form-item>-->
-<!--        <el-form-item label="Remark">-->
-<!--          <el-input v-model="temp.remark" :autosize="{ minRows: 2, maxRows: 4}" type="textarea" placeholder="Please input" />-->
-<!--        </el-form-item>-->
-      </el-form>
+        <el-form ref="dataForm" :rules="rules" :model="temp" label-position="right" label-width="110px" style="width: 500px; margin-left:30px;">
+  <!--        <el-form-item label="Type" prop="type">-->
+  <!--          <el-select v-model="temp.type" class="filter-item" placeholder="Please select">-->
+  <!--            <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />-->
+  <!--          </el-select>-->
+  <!--        </el-form-item>-->
+  <!--        <el-form-item label="Date" prop="timestamp">-->
+  <!--          <el-date-picker v-model="temp.timestamp" type="datetime" placeholder="Please pick a date" />-->
+  <!--        </el-form-item>-->
+          <el-form-item label="弹窗名称" prop="name">
+            <el-input v-model="temp.name" />
+          </el-form-item>
+          <el-form-item label="弹窗类型" prop="winType">
+  <!--          <el-radio-group v-model="ruleForm.resource">-->
+            <el-radio-group v-model="temp.winType" @change="winTypeChange">
+              <el-radio  :label="0">静态</el-radio>
+              <el-radio  :label="1">动态</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item v-show="isShow" label="关联动态弹窗">
+            <el-input v-model="temp.relatedId" />
+          </el-form-item>
+          <el-form-item label="用户类型">
+  <!--          <el-input v-model="temp.userType" />-->
+            <el-select v-model="temp.userType" placeholder="请选择用户类型" style="width: 390px;">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="弹窗场景" prop="scene">
+            <el-input v-model="temp.scene" />
+          </el-form-item>
+          <el-form-item label="弹窗次数">
+            <el-input v-model="temp.time" />
+          </el-form-item>
+          <el-form-item label="频率（天）">
+            <el-input placeholder="0表示不限制频率" v-model="temp.frequency" />
+          </el-form-item>
+          <el-form-item label="优先级" prop="priority">
+            <el-input v-model="temp.priority" />
+          </el-form-item>
+          <el-form-item label="UI样式">
+            <el-input v-model="temp.imgUrl" />
+          </el-form-item>
+          <el-form-item label="跳转地址">
+            <el-input v-model="temp.desLink" />
+          </el-form-item>
+          <el-form-item label="总弹窗次数">
+            <el-input v-model="temp.totalTime" />
+          </el-form-item>
+          <el-form-item label="前端数据">
+            <el-input v-model="temp.data" />
+          </el-form-item>
+          <el-form-item label="最低可见版本号">
+            <el-input placeholder="此版本号以下无法看见此数据" v-model="temp.lowVersion" />
+          </el-form-item>
+          <el-form-item label="最高可见版本号">
+            <el-input placeholder="此版本号以上无法看见此数据" v-model="temp.highVersion" />
+          </el-form-item>
+  <!--        </el-form-item>-->
+  <!--        <el-form-item label="Status">-->
+  <!--          <el-select v-model="temp.status" class="filter-item" placeholder="Please select">-->
+  <!--            <el-option v-for="item in statusOptions" :key="item" :label="item" :value="item" />-->
+  <!--          </el-select>-->
+  <!--        </el-form-item>-->
+  <!--        <el-form-item label="Imp">-->
+  <!--          <el-rate v-model="temp.importance" :colors="['#99A9BF', '#F7BA2A', '#FF9900']" :max="3" style="margin-top:8px;" />-->
+  <!--        </el-form-item>-->
+  <!--        <el-form-item label="Remark">-->
+  <!--          <el-input v-model="temp.remark" :autosize="{ minRows: 2, maxRows: 4}" type="textarea" placeholder="Please input" />-->
+  <!--        </el-form-item>-->
+        </el-form>
       </div>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
@@ -234,7 +246,7 @@
           '3': '前7天下过美团订单，当天未下美团订单的用户',
           '4': '前7天下过饿了么订单，当天未下饿了么订单的用户',
           '5': '可提现金额＜1的用户，近7天未下单',
-          '6': '1≤订单量≤4的用户',
+          '6': '1≤订单量≤4，且5单红包任务未失效的用户',
           '7': '近1个月有积分兑换的用户',
           '8': '近1个月使用过购物红包的用户',
           '9': '积分值＜500的用户'
@@ -245,7 +257,61 @@
     //保存组件内所有数据
     data() {
       return {
+        temp: {
+          id: undefined,
+          importance: 1,
+          remark: '',
+          timestamp: new Date(),
+          title: '',
+          type: '',
+          status: 'published',
+          winType: 0,
+          name:''
+        },
+        //表单校验规则
+        rules: {
+          name: [{ required: true, message: '弹窗名称不能为空', trigger: 'blur' }],
+          winType: [{ type: 'date', required: true, message: 'timestamp is required', trigger: 'change' }],
+          scene: [{ required: true, message: 'title is required', trigger: 'blur' }],
+          priority: [{ required: true, message: '弹窗优先级不能为空', trigger: 'blur' }]
+        },
+        //表单用户类型数据
+        options: [{
+          value: '0',
+          label: '所有用户'
+        }, {
+          value: '1',
+          label: '有0元购资格，且资格有效期在3天之内的用户'
+        }, {
+          value: '2',
+          label: '购物红包有效期剩3天的用户'
+        }, {
+          value: '3',
+          label: '前7天下过美团订单，当天未下美团订单的用户'
+        }, {
+          value: '4',
+          label: '前7天下过饿了么订单，当天未下饿了么订单的用户'
+        }, {
+          value: '5',
+          label: '可提现金额＜1的用户，近7天未下单'
+        }, {
+          value: '6',
+          label: '1≤订单量≤4，且5单红包任务未失效的用户'
+        }, {
+          value: '7',
+          label: '近1个月有积分兑换的用户'
+        }, {
+          value: '8',
+          label: '近1个月使用过购物红包的用户'
+        }, {
+          value: '9',
+          label: '积分值＜500的用户'
+        }],
+        value: '',
+        //弹窗类型单选框默认值
+        radio: 0,
         total: 0,
+        isShow: false,
         datalist: {
           list: [],
           listLoading: true
@@ -262,10 +328,7 @@
         dialogStatus: '',
         textMap: {
           update: 'Edit',
-          create: 'Create'
-        },
-        temp: {
-          title: ''
+          create: '新增弹窗'
         }
       }
     },
@@ -294,13 +357,25 @@
           // })
         })
       },
+      //重置表单（temp就是表单的model）
+      resetTemp() {
+        this.temp.title = '' // 这么写啊！
+      },
       handleCreate() {
+        this.resetTemp()
         this.dialogStatus = 'create'
         this.dialogFormVisible = true
-        this.$nextTick(() => {
-          this.$refs['dataForm'].clearValidate()
-        })
+        // this.$nextTick(() => {
+        //   this.$refs['dataForm'].clearValidate()
+        // })
+      },
+      //弹窗类型监听
+      winTypeChange(val) {
+        this.isShow = val;
       }
     }
   }
 </script>
+
+<style scoped>
+</style>
